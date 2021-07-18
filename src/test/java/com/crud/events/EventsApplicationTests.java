6af32579782f5f5f2be9e8e70f8e6dd3228eb52e
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2021-07-18 11:03:50 +0200

    First commit

---
+++ src/test/java/com/crud/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
